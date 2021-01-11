dfbea9381b6d26e7f3577a2fb2f7866810d3ee9c
Shroomsher <44953328+Shroomsher@users.noreply.github.com>
2021-01-10 22:05:56 -0500

    kill pt 2

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def sigmoid_derivative(x):
     return x * (1 - x)
 
+
 np.random.seed(1)
 
-training_outputs = np.random.rand(3000)
+training_outputs = np.random.rand(1000)
 
 random_array = np.random.rand(3000)
 rounded_array = np.around(random_array, decimals=0)
-reshaped_array = rounded_array.reshape(1000,3)
+reshaped_array = rounded_array.reshape(1000, 3)
 training_inputs = reshaped_array
 
 for iteration in range(1000):
-    if reshaped_array[iteration,0] == 1:
+    if reshaped_array[iteration, 0] == 1:
         training_outputs[iteration] = 1
     else:
         training_outputs[iteration] = 0
 
-synaptic_weights = 2 * np.random.random((3, 1)) - 1
+synaptic_weights = 2 * np.random.random(3) - 1
 
 print(f"Random start synaptic weights:\n{synaptic_weights}")
 
-for iteration in range(1000):
+for iteration in range(10000):
 
     input_layer = training_inputs
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
@@ -40,4 +43,8 @@ for iteration in range(1000):
 
 print(f"Synaptic weights after training:\n{synaptic_weights}")
 
-print(f"Outputs after training:\n{outputs}")
+# print(f"Outputs after training:\n{outputs}")
+
+# print("Inputs: " + str(input_layer))
+
+print("Average Error: " + str(np.average(error)))
